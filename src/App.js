60cc4637fcf7356445cdf49c6d244b91596b7331
qcalmel <contact@quentincalmel.fr>
2022-06-16 20:16:00 +0200

    feat(home) add routing to app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import BottomNavBar from "./components/BottomNavBar";
 import TopAppBar from "./components/TopAppBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Calendar from "./components/Calendar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <TopAppBar />
-      <BottomNavBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <TopAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+        <BottomNavBar />
+      </div>
+    </BrowserRouter>
   );
 }
 
