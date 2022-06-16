67be8fc5dc2d6ff920b2043cbbf36a4096cc4c69
qcalmel <contact@quentincalmel.fr>
2022-06-16 20:16:41 +0200

    feat(BottomNavBar) add link to BottomNavBar

--- src/components/BottomNavBar.js
+++ src/components/BottomNavBar.js
@@ -1,8 +1,9 @@
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
 import CalendarIcon from "@mui/icons-material/CalendarMonth";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const BottomNavBar = () => {
   const [value, setValue] = useState(0);
@@ -17,8 +18,12 @@ const BottomNavBar = () => {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction icon={<SearchIcon />} />
-        <BottomNavigationAction icon={<CalendarIcon />} />
+        <BottomNavigationAction component={Link} to="/" icon={<HomeIcon />} />
+        <BottomNavigationAction
+          component={Link}
+          to="/calendar"
+          icon={<CalendarIcon />}
+        />
         <BottomNavigationAction icon={<LocationOnIcon />} />
       </BottomNavigation>
     </Paper>
