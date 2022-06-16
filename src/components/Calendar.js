ec304361a83365c043124914c23997beb2757e9c
qcalmel <contact@quentincalmel.fr>
2022-06-16 20:15:19 +0200

    feat(calendar) create calendar page component

---
+++ src/components/Calendar.js
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Calendar = () => {
+  return (
+    <Typography sx={{ textAlign: "center" }} variant="h1">
+      Calendar
+    </Typography>
+  );
+};
+
+export default Calendar;
